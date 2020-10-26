8661dcadd8dbf6fe58bd23ec2f7c8f515b818289
rchacoma8 <rchacoma8@alumnes.ub.edu>
2020-10-26 20:55:14 +0100

    US14T1 feat: Added a view to list the available motos

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Motos from '@/components/Motos.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/Motos',
+      name: 'Motos',
+      component: Motos
     }
   ]
 })
