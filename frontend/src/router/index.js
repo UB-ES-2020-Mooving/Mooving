46a9261b6f082f00d6596b24cf98308aa4a1ff76
lwangwan <U0.0UU0.0UQ.Q@gmail.com>
2020-10-28 11:24:14 +0100

    Create Login Page and some basic validation

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Registration from '@/components/Registration.vue'
+import Login from '@/components/Login.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/userregister',
       name: 'Registration',
       component: Registration
+    },
+    {
+      path: '/userlogin',
+      name: 'Login',
+      component: Login
     }
   ]
 })
