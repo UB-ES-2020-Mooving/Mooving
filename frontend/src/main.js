19153d155ba0aec8ff2d34bdf999ff32f624a6cf
ireneGarciaFernandez <31886095+ireneGarciaFernandez@users.noreply.github.com>
2020-10-28 11:44:02 +0100

    Homepage implementada

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import '@/../bootstrap/css/bootstrap.css'
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: (h) => h(App)
+}).$mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Homepage from '@/components/Homepage'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/Homepage',
+      name: 'Homepage',
+      component: Homepage
     }
   ]
 })
