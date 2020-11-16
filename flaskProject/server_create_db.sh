21681bf13034b9257e325260e56691225a6a76a5
edumargra <emartigr10@alumnes.ub.edu>
2020-11-16 17:39:11 +0100

    reset psql

--- flaskProject/server_create_db.sh
+++ flaskProject/server_create_db.sh
@@ -10,7 +10,8 @@ fi
 if [ -f "data.db" ]; then
     rm "data.db"
 fi
-
+psql -d $DATABASE_URL -c "DROP TABLE alembic_version ;"
+export FLASK_APP=app
 flask db init
 flask db migrate -m "Initial migration"
 flask db upgrade
