3e073d9749a07cefaf29ce87c7f362b0903158bb
edumargra <emartigr10@alumnes.ub.edu>
2020-11-16 17:47:07 +0100

    run heroku scripts

--- flaskProject/server_create_db.sh
+++ flaskProject/server_create_db.sh
@@ -11,7 +11,6 @@ if [ -f "data.db" ]; then
     rm "data.db"
 fi
 psql -d $DATABASE_URL -c "DROP TABLE alembic_version ;"
-export FLASK_APP=app
 flask db init
 flask db migrate -m "Initial migration"
 flask db upgrade
