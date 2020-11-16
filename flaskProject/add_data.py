5b38a28bdf85af4712d199fad2f5d286dcdf0837
Eduard <eduard.margra@gmail.com>
2020-11-16 21:25:25 +0100

    Change default db to False
    Use of sqlite instead of psql

--- flaskProject/add_data.py
+++ flaskProject/add_data.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-if config('PRODUCTION', cast=bool, default=True):
+if config('PRODUCTION', cast=bool, default=False):
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL', default='localhost')
 
 db = SQLAlchemy(app)
