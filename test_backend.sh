776f0d00a6a359039b60eac7647031bc4c0a1c44
edumargra <emartigr10@alumnes.ub.edu>
2020-12-01 16:59:12 +0100

    Run backend tests with travis

---
+++ test_backend.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -ev
+
+if [[ -z "$TEST_BACKEND" ]]; then
+    echo "No backend tests to run, exiting..."
+    exit 0
+fi
+
+cd flaskProject
+echo "Installing depencies"
+pip install -r requirements.txt
+echo "Running tests..."
+case $TEST_BACKEND in
+    SECONDARY)
+        python -m pytest tests/tests_secondary
+        ;;
+    ENDPOINTS)
+        python -m pytest tests/tests_endpoints
+        ;;
+    ALMOST_ALL)
+        python -m pytest tests/tests_auto
+        python -m pytest tests/tests_secondary
+        ;;
+    *)
+        python -m pytest tests/
+        ;;
+esac
+echo "Tests done"
+exit 0
---
+++ test_frontend.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ev
+
+if [[ -z "$TEST_FRONTEND" ]]; then
+    echo "No frontend tests to run, exiting..."
+    exit 0
+fi
+
+cd $FRONTEND
+echo "Running tests..."
+npm run test:unit
+echo "Tests done"
+exit 0
