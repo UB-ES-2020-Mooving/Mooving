6accc08490a2d7a369b558b25965d15996e123a0
edumargra <emartigr10@alumnes.ub.edu>
2020-12-02 01:29:40 +0100

    test reserve moto

--- flaskProject/tests/tests_auto/test_moto_reserve.py
+++ flaskProject/tests/tests_auto/test_moto_reserve.py
@@ -4,11 +4,23 @@ from tests.tests_auto.config_tests import *
 def test_moto_reserve():
 
     with app.test_client() as c:
+        # Path GET moto reserve (client_email = juanita@gmail.com)
+        path = '/reserve/juanita@gmail.com'
+
+        r = c.get(path)
+
+        json_data_get = r.get_json()
+
+        # Comprobamos si el estatus code es 201
+        assert (r.status_code == 500 or  r.status_code == 404 or r.status_code == 201)
+
+        # Comprobamos que el encabezado del json sea el adecuado
+        assert "message" in json_data_get.keys()
 
         # Path POST moto reserve (client_email = juanita@gmail.com / moto_id = 1)
         r = c.post('/reserve', json={
-            "client_id": "juanita@gmail.com",
-            "moto_id": "2"
+            "client_email": "juanita@gmail.com",
+            "moto_id": 1
             })
         json_data_post = r.get_json()
 
@@ -17,8 +29,7 @@ def test_moto_reserve():
 
         # Comprobamos que el encabezado del json sea el adecuado
         list_keys = ['message', 'remaining_time']
-        assert "reserved_moto" in json_data_post.keys()
-
+        assert list(json_data_post.keys()) == list_keys
 
         # Path GET moto reserve (client_email = juanita@gmail.com)
         path = '/reserve/juanita@gmail.com'
@@ -37,5 +48,5 @@ def test_moto_reserve():
         json_body = json_data_get["reserved_moto"]
 
         # Comprobamos los valores del Json
-        list_keys = ['id', 'matricula', 'model_generic', 'km_restantes', 'address', 'last_coordinate_latitude', 'last_coordinate_longitude', 'address']
+        list_keys = ['id', 'matricula', 'model_generic', 'km_restantes', 'address', 'last_coordinate_latitude', 'last_coordinate_longitude', 'distance']
         assert list(json_body.keys()) == list_keys
