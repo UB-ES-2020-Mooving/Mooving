4745d463ced7b461d72e82ec9023757f3e9697d4
ireneGarciaFernandez <31886095+ireneGarciaFernandez@users.noreply.github.com>
2020-12-05 16:47:27 +0100

    Merge branch 'develop' into TD1T1_ModifyImageMotorbikes

--- frontend/src/components/clientMoto.vue
+++ frontend/src/components/clientMoto.vue
@@ -51,7 +51,7 @@
               <hr>
               <div class="row">
                 <div class="col-sm-3">
-                  <h6 class="mb-0">Fuel/Battery</h6>
+                  <h6 class="mb-0">Battery</h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
                   {{ this.moto.km_restantes }} km
