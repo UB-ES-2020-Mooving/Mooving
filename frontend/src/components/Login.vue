9c360a513a67ca64e3bab9d78b09388f89622edc
rchacoma8 <rchacoma8@alumnes.ub.edu>
2020-12-05 16:21:49 +0100

    Style: Make uniform colour background for login and register pages

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,31 +1,34 @@
 <template>
-  <div class="jumbotron">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-8 offset-sm-2">
-          <div>
-            <h2>Log in</h2>
-            <form @submit.prevent="handleSubmit">
-              <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" v-model="user.email" id="email" name="email" class="form-control" :class="{ 'is-invalid': submitted && $v.user.email.$error }" />
-                <div v-if="submitted && $v.user.email.$error" class="invalid-feedback">
-                  <span v-if="!$v.user.email.required">Email is required</span>
-                  <span v-if="!$v.user.email.email">Email is invalid</span>
+  <div id="wholePage">
+    <!-- Divisor que ocupa solo lo necesario para cubrir la info del login-->
+    <div class="jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 offset-sm-2">
+            <div>
+              <h2>Log in</h2>
+              <form @submit.prevent="handleSubmit">
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input type="email" v-model="user.email" id="email" name="email" class="form-control" :class="{ 'is-invalid': submitted && $v.user.email.$error }" />
+                  <div v-if="submitted && $v.user.email.$error" class="invalid-feedback">
+                    <span v-if="!$v.user.email.required">Email is required</span>
+                    <span v-if="!$v.user.email.email">Email is invalid</span>
+                  </div>
                 </div>
-              </div>
-              <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" v-model="user.password" id="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && $v.user.password.$error }" />
-                <div v-if="submitted && $v.user.password.$error" class="invalid-feedback">
-                  <span v-if="!$v.user.password.required">Password is required</span>
-                  <span v-if="!$v.user.password.minLength">Password must be at least 6 characters</span>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" v-model="user.password" id="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && $v.user.password.$error }" />
+                  <div v-if="submitted && $v.user.password.$error" class="invalid-feedback">
+                    <span v-if="!$v.user.password.required">Password is required</span>
+                    <span v-if="!$v.user.password.minLength">Password must be at least 6 characters</span>
+                  </div>
                 </div>
-              </div>
-              <div class="form-group">
-                <button class="btn btn-primary">Log in</button>
-              </div>
-            </form>
+                <div class="form-group">
+                  <button class="btn btn-primary">Log in</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -91,5 +94,18 @@ export default {
 </script>
 
 <style scoped>
+#wholePage
+  {
+  position:fixed;
+  padding:0;
+  margin:0;
 
+  top:0;
+  left:0;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: #e9ecef;
+  }
 </style>
