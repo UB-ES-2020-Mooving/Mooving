afe135b1a506f81490040fb8644fd2c049806ea2
ireneGarciaFernandez <31886095+ireneGarciaFernandez@users.noreply.github.com>
2020-12-06 18:51:28 +0100

    Añadidas decoraciones varias

--- frontend/src/components/AddMotoForm.vue
+++ frontend/src/components/AddMotoForm.vue
@@ -1,42 +1,49 @@
 <template>
-  <div class="jumbotron">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-8 offset-sm-2">
-          <div>
-            <h2>Motorbike Form</h2>
-            <form @submit.prevent="handleSubmit">
-              <div class="form-group">
-                <label for="licensePlate">License Plate</label>
-                <input type="text" v-model="moto.licensePlate" id="licensePlate" name="licensePlate" class="form-control" :class="{ 'is-invalid': submitted && $v.moto.licensePlate.$error }" />
-                <div v-if="submitted && $v.moto.licensePlate.$error" class="invalid-feedback">
-                  <span v-if="!$v.moto.licensePlate.required">License Plate is required</span>
-                  <span v-if="!$v.moto.licensePlate.minLength">License Plate must be at least 8 characters</span>
+  <div id="wholePage">
+    <b-navbar toggleable type="dark" variant="dark">
+        <b-navbar-brand href="#">
+          <img src="./Images/moovingLogoBlanco.png" alt= "Logo" style= "width:100px;">
+        </b-navbar-brand>
+    </b-navbar>
+    <div class="jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 offset-sm-2">
+            <div>
+              <h2>Motorbike Form</h2>
+              <form @submit.prevent="handleSubmit">
+                <div class="form-group">
+                  <label for="licensePlate">License Plate</label>
+                  <input type="text" v-model="moto.licensePlate" id="licensePlate" name="licensePlate" class="form-control" :class="{ 'is-invalid': submitted && $v.moto.licensePlate.$error }" />
+                  <div v-if="submitted && $v.moto.licensePlate.$error" class="invalid-feedback">
+                    <span v-if="!$v.moto.licensePlate.required">Please, complete the form</span>
+                    <span v-if="!$v.moto.licensePlate.minLength">License Plate must be at least 8 characters</span>
+                  </div>
                 </div>
-              </div>
-              <div class="form-group">
-                <label for="type">Type</label>
-                <select v-model="moto.type" id="type" name="type" class="form-control" :class="{ 'is-invalid': submitted && $v.moto.type.$error }" >
-                  <option disabled value="">Select type</option>
-                  <option>Basic</option>
-                  <option>Premium</option>
-                </select>
-                <div v-if="submitted && $v.moto.type.$error" class="invalid-feedback">
-                  <span v-if="!$v.moto.type.required">Type is required</span>
+                <div class="form-group">
+                  <label for="type">Type</label>
+                  <select v-model="moto.type" id="type" name="type" class="form-control" :class="{ 'is-invalid': submitted && $v.moto.type.$error }" >
+                    <option disabled value="">Select type</option>
+                    <option>Basic</option>
+                    <option>Premium</option>
+                  </select>
+                  <div v-if="submitted && $v.moto.type.$error" class="invalid-feedback">
+                    <span v-if="!$v.moto.type.required">Please, complete the form</span>
+                  </div>
                 </div>
-              </div>
-              <div class="form-group">
-                <button class="btn"
-                    id="cancelButton"
-                    type="button"
-                    @click="cancel()"
-                    style="margin-top: 20px;margin-left: 10px;border-radius: 12px;
-                    background-color: #ff6961;color: #ffffff; width: 140px">
-                  Cancel
-                </button>
-                <button class="btn btn-primary" style="margin-top: 20px;margin-left: 5px;border-radius: 12px; background-color: #343a40;color: #42b983; width: 140px">Confirm</button>
-              </div>
-            </form>
+                <div class="form-group">
+                  <button class="btn"
+                      id="cancelButton"
+                      type="button"
+                      @click="cancel()"
+                      style="margin-top: 20px;margin-left: 10px;border-radius: 12px;
+                      background-color: #ff6961;color: #ffffff; width: 140px">
+                    Cancel
+                  </button>
+                  <button class="btn btn-primary" style="margin-top: 20px;margin-left: 5px;border-radius: 12px; background-color: #343a40;color: #42b983; width: 140px">Confirm</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -49,7 +56,7 @@ import { required, email, minLength, maxLength, sameAs } from 'vuelidate/lib/val
 import axios from 'axios'
 
 export default {
-  name: 'Registration',
+  name: 'MotoForm',
   data () {
     return {
       email: '',
@@ -80,7 +87,6 @@ export default {
       }
     },
     cancel(){
-      //alert('cancel')
       this.$router.push({ path: '/motospagemechanic', query: { email: this.email } })
     }
   }
@@ -88,5 +94,15 @@ export default {
 </script>
 
 <style scoped>
-
+#wholePage
+  {
+  position:fixed;
+  padding:0;
+  margin:0;
+  top:0;
+  left:0;
+  width: 100%;
+  height: 100%;
+  background-color: #e9ecef;
+  }
 </style>
