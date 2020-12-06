d9fbad8bc2d1379466ce463d593d64654fb41880
ireneGarciaFernandez <31886095+ireneGarciaFernandez@users.noreply.github.com>
2020-12-06 18:27:24 +0100

    License Plate acabado

--- frontend/src/components/AddMotoForm.vue
+++ frontend/src/components/AddMotoForm.vue
@@ -8,7 +8,7 @@
             <form @submit.prevent="handleSubmit">
               <div class="form-group">
                 <label for="licensePlate">License Plate</label>
-                <input type="text" v-model="moto.licensePlate" id="licensePlate" name="LicensePlate" class="form-control" :class="{ 'is-invalid': submitted && $v.moto.licensePlate.$error }" />
+                <input type="text" v-model="moto.licensePlate" id="licensePlate" name="licensePlate" class="form-control" :class="{ 'is-invalid': submitted && $v.moto.licensePlate.$error }" />
                 <div v-if="submitted && $v.moto.licensePlate.$error" class="invalid-feedback">
                   <span v-if="!$v.moto.licensePlate.required">License Plate is required</span>
                   <span v-if="!$v.moto.licensePlate.minLength">License Plate must be at least 8 characters</span>
@@ -23,13 +23,7 @@
                     background-color: #ff6961;color: #ffffff; width: 140px">
                   Cancel
                 </button>
-                <button class="btn"
-                    id="confirmButton"
-                    type="button"
-                    style="margin-top: 20px;margin-left: 10px;border-radius: 12px;
-                    background-color: #343a40;color: #42b983; width: 140px">
-                  Confirm
-                </button>
+                <button class="btn btn-primary" style="margin-top: 20px;margin-left: 5px;border-radius: 12px; background-color: #343a40;color: #42b983; width: 140px">Confirm</button>
               </div>
             </form>
           </div>
@@ -40,44 +34,41 @@
 </template>
 
 <script>
-import { required, minLength } from 'vuelidate/lib/validators'
+import { required, email, minLength, maxLength, sameAs } from 'vuelidate/lib/validators'
 import axios from 'axios'
 
 export default {
-  name: 'MotoForm',
+  name: 'Registration',
   data () {
     return {
+      email: '',
       moto: {
         licensePlate: '',
-        type: ''
       },
       submitted: false
     }
   },
   validations: {
-    moto:{
-      licensePlate: { required, minLength: minLength(8) },
-      type: { required }
+    moto: {
+      licensePlate: { required, minLength: minLength(8) }
     }
   },
   created () {
     this.email = this.$route.query.email
   },
   methods: {
-    cancel(){
-      alert('cancel')
-      this.$router.push({ path: '/motospagemechanic', query: { email: this.email } })
-    },
     handleSubmit (e) {
       this.submitted = true
-      alert('ha entrado')
 
       // stop here if form is invalid
       this.$v.$touch()
       if (this.$v.$invalid) {
-        alert('ERROR')
         return
       }
+    },
+    cancel(){
+      //alert('cancel')
+      this.$router.push({ path: '/motospagemechanic', query: { email: this.email } })
     }
   }
 }
