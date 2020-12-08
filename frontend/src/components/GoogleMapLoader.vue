37b336c3fbffab91a22b16fbd171c302c0de6e64
ireneGarciaFernandez <31886095+ireneGarciaFernandez@users.noreply.github.com>
2020-12-08 02:04:25 +0100

    Add markers
    Ya tenemos los tres markers distintos (naranja,verde y lila)

--- frontend/src/components/GoogleMapLoader.vue
+++ frontend/src/components/GoogleMapLoader.vue
@@ -13,7 +13,25 @@
           :key="index"
           v-for="(pin, index) in markers"
           :position="pin.position"
-          :icon="pin.icon"
+          :icon="{ url: require('./Images/orange-dot.png')}"
+          :clickable="true"
+          :draggable="true"
+          @click="center=pin.position"> //:icon="{ url: require('./Images/iconNormal.png')}"
+        </GmapMarker>
+        <GmapMarker
+          :key="index"
+          v-for="(pin, index) in markers2"
+          :position="pin.position"
+          :icon="{ url: require('./Images/green-dot.png')}"
+          :clickable="true"
+          :draggable="true"
+          @click="center=pin.position">
+        </GmapMarker>
+        <GmapMarker
+          :key="index"
+          v-for="(pin, index) in markers3"
+          :position="pin.position"
+          :icon="{ url: require('./Images/purple-dot.png')}"
           :clickable="true"
           :draggable="true"
           @click="center=pin.position">
@@ -25,6 +43,7 @@
 
 <script>
 const home = { lat: 41.417988, lng: 2.185246 };
+const home2 = { lat: 41.41793, lng: 2.18523 };
 export default {
   data() {
     return {
@@ -32,12 +51,15 @@ export default {
       // change this to whatever makes sense
       center: { lat: 45.508, lng: -73.587 },
       markers: [],
+      markers2: [],
+      markers3: [],
       places: [],
       currentPlace: null,
       myCoordinates: {
           lat: 0,
           ln: 0
-      }
+      },
+      m: true
     };
   },
   mounted() {
@@ -63,6 +85,22 @@ export default {
           },
           {
             position: {lat: 41.43, lng: 2.186}
+          }
+        ],
+        this.markers2 = [
+          {
+            position: home2,
+          },
+          {
+            position: {lat: 41.432, lng: 2.182}
+          }
+        ],
+        this.markers3 = [
+          {
+            position: {lat: 41.431, lng: 2.181},
+          },
+          {
+            position: {lat: 41.4325, lng: 2.1825}
           }
         ]
     },
