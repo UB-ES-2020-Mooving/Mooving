591efcb07a1e8a8ad470558191c038e7a1a0a49c
ireneGarciaFernandez <31886095+ireneGarciaFernandez@users.noreply.github.com>
2020-12-08 23:23:49 +0100

    space rectified

--- frontend/src/components/AddMotoForm.vue
+++ frontend/src/components/AddMotoForm.vue
@@ -87,7 +87,7 @@ export default {
       }
       this.$router.push({ path: '/motospagemechanic', query: { email: this.email } })
     },
-    cancel(){
+    cancel () {
       this.$router.push({ path: '/motospagemechanic', query: { email: this.email } })
     }
   }
