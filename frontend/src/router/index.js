e984d134fd09c49716a14027d815b63a997f026f
edumargra <emartigr10@alumnes.ub.edu>
2020-12-09 16:27:39 +0100

    Add modify account form

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,6 +10,7 @@ import clientMoto from '../components/clientMoto'
 import mechanicMoto from '../components/MechanicMoto'
 import motoForm from '../components/AddMotoForm'
 import ConfirmDeleteAccountDialog from '@/components/ConfirmDeleteAccountDialog.vue'
+import ModifyAccountForm from '@/components/ModifyAccountForm.vue'
 
 
 Vue.use(Router)
@@ -67,6 +68,11 @@ export default new Router({
       path: '/confirmDeleteAccount',
       name: 'confirmDeleteAccount',
       component: ConfirmDeleteAccountDialog
+    },
+    {
+      path: '/modifyAccountForm',
+      name: 'modifyAccountForm',
+      component: ModifyAccountForm
     }
   ]
 })
