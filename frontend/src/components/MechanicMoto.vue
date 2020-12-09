10a33d63e012dd25380cc7124e08d0cb65932ddd
rchacoma8 <rchacoma8@alumnes.ub.edu>
2020-12-09 13:06:43 +0100

    Feat: add nav bar for mechanic in motos list and info pages

--- frontend/src/components/MechanicMoto.vue
+++ frontend/src/components/MechanicMoto.vue
@@ -5,8 +5,15 @@
         <b-navbar-brand href="#">
           <img src="./Images/moovingLogoBlanco.png" alt= "Logo" style= "width:100px;">
         </b-navbar-brand>
+        <b-navbar-toggle target="navbar-toggle-collapse">
+          <template #default="{ expanded }">
+            <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+            <b-icon v-else icon="chevron-bar-down"></b-icon>
+          </template>
+        </b-navbar-toggle>
         <b-collapse id="navbar-toggle-collapse" is-nav>
           <b-navbar-nav class="ml-auto">
+            <b-nav-item style="text-align: center"><router-link :to="{path: '/motospagemechanic', query: { email: this.email } }">Motorbikes</router-link></b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
