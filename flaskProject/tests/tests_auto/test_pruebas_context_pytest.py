cb25f0a528f794ac6292a962323321cf0c0b6653
edumargra <emartigr10@alumnes.ub.edu>
2020-12-11 00:46:14 +0100

    clean try-and-error files

--- flaskProject/tests/tests_auto/test_pruebas_context_pytest.py
+++
@@ -1,9 +0,0 @@
-from tests.tests_auto.config_tests import *
-
-def test_get_list(client):
-    c = client
-    path = "/clients"
-    r = c.get(path)
-    json_data = r.get_json()
-
-    assert 4 == 5
