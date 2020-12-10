1d08366f28764f4e0cad3cc86ab180b77e2889d1
ireneGarciaFernandez <31886095+ireneGarciaFernandez@users.noreply.github.com>
2020-12-10 18:12:29 +0100

    pasito a pasito

--- frontend/tests/unit/profile.spec.js
+++ frontend/tests/unit/profile.spec.js
@@ -5,16 +5,30 @@ import Profile from '../../src/components/Profile'
 const $route = {
   query: { email: 'juanita@gmail.com' }
 }
+jest.mock('axios', () => ({
+  get: () => Promise.resolve()(
+      {
+        data: {
+          client_profile: {
+            nombre: 'Juana',
+            iban: '2223462362665251w',
+            dni_nie: '11111111J',
+            email: 'juanita@gmail.com'
+          }
+        }
+      }
+  )
+}))
 // eslint-disable-next-line no-undef
 describe('Profile Page', () => {
   // eslint-disable-next-line no-undef
-  it('only displays data ', () => {
+  it('has the required elements', () => {
     const wrapper = shallowMount(Profile, {
       mocks: {
           $route
         }
     })
-    wrapper.setData(({ email: "juanita@gmail.com"}))
+    wrapper.setData(({ email: "juanita@gmail.com" }))
     // eslint-disable-next-line no-undef
     expect(wrapper.findAll("h6").filter(node => node.text().match("Name")).exists()).toBe(true)
     // eslint-disable-next-line no-undef
@@ -23,5 +37,22 @@ describe('Profile Page', () => {
     expect(wrapper.findAll("h6").filter(node => node.text().match("IBAN")).exists()).toBe(true)
     // eslint-disable-next-line no-undef
     expect(wrapper.findAll("h6").filter(node => node.text().match("DNI/NIE")).exists()).toBe(true)
+  })
+})
+describe('User gets their data ', () => {
+  // eslint-disable-next-line no-undef
+  it('has the correct output', async () => {
+    const wrapper = shallowMount(Profile, {
+      mocks: {
+        $route
+      }
+    })
+    await flushPromises()
+    await wrapper.setData(({ email: "juanita@gmail.com" }))
+    await flushPromises()
+    expect(wrapper.nombre).toBe('Juana')
+    expect(wrapper.iban).toBe('2223462362665251w')
+    expect(wrapper.dni_nie).toBe('11111111J')
+    expect(wrapper.email).toBe('juanita@gmail.com')
   })
 })
