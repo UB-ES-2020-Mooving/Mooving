9afe69a473d9effa83f7c19a99d3b060f233ec79
edumargra <emartigr10@alumnes.ub.edu>
2020-12-11 00:41:56 +0100

    Use the fixture correctly

--- flaskProject/tests/tests_auto/test_pruebas_context_pytest.py
+++ flaskProject/tests/tests_auto/test_pruebas_context_pytest.py
@@ -1,9 +1,9 @@
 from tests.tests_auto.config_tests import *
 
 def test_get_list(client):
-    with client as c:
-        path = "/clients"
-        r = c.get(path)
-        json_data = r.get_json()
+    c = client
+    path = "/clients"
+    r = c.get(path)
+    json_data = r.get_json()
 
-        assert len(json_data("clients")) == 4
+    assert 4 == 5
