f0500ccdd2ead85102bef3239bf97d59074b2698
Jose Blanco Fernandez <31952475+SrLimoncio@users.noreply.github.com>
2020-12-14 19:32:29 +0100

    update

--- flaskProject/tests/tests_auto/test_profile_modify.py
+++ flaskProject/tests/tests_auto/test_profile_modify.py
@@ -1,100 +1,101 @@
-
 from tests.tests_auto.config_tests import *
 from models.client_model import ClientModel
 
-def test_profile_modify_correct():
-    with app.test_client() as c:
+def test_profile_modify_correct(client):
+    c = client
+    # Cogemos un cliente random de la base de datos.
+    client_old = ClientModel.query.filter(ClientModel.client_id == 1).first()
 
-        # Cogemos un cliente random de la base de datos.
-        client_old = ClientModel.query.filter(ClientModel.client_id == 1).first()
+    # Nos inventamos datos
+    new_email = "inventado@inventado.com"
+    new_dni_nie = 000000000000
+    new_name = "Inventado"
+    new_iban = 0000000000000000000000
 
-        # Nos inventamos datos
-        new_email = "inventado@inventado.com"
-        new_dni_nie = 000000000000
-        new_name = "Inventado"
-        new_iban = 0000000000000000000000
+    # Nos aseguramos de que no exista ese email en la base de datos
+    assert ClientModel.find_by_email(new_email) is None
 
-        # Nos aseguramos de que no exista ese email en la base de datos
-        assert ClientModel.find_by_email(new_email) is None
+    # Nos aseguramos de que no exista ese dni en la base de datos
+    assert ClientModel.find_by_dni(new_dni_nie) is None
 
-        # Nos aseguramos de que no exista ese dni en la base de datos
-        assert ClientModel.find_by_dni(new_dni_nie) is None
+    # Hacemos el put con datos inventados
+    path = '/profile' + client_old.email
+    params = {
+        'name': new_name,
+        'iban': new_iban,
+        'dni_nie': new_dni_nie,
+        'email': new_email
+    }
+    r = c.put(path, json=params)
 
-        # Hacemos el put con datos inventados
-        path = '/profile' + client_old.email
-        params = {
-            'name': new_name,
-            'iban': new_iban,
-            'dni_nie': new_dni_nie,
-            'email': new_email
-        }
-        r = c.put(path, json=params)
+    assert r.status_code == 200
 
-        assert r.status_code == 200
+    # Antes de nada comprobamos si realmente se han modificado el email o el dni
+    assert ClientModel.find_by_email(new_email) is not None
 
-        # Antes de nada comprobamos si realmente se han modificado el email o el dni
-        assert ClientModel.find_by_email(new_email) is not None
+    assert ClientModel.find_by_dni(new_dni_nie) is not None
 
-        assert ClientModel.find_by_dni(new_dni_nie) is not None
+    # Ahora comprobaremos los otros datos
+    new_client = ClientModel.find_by_email(new_email)
 
-        # Ahora comprobaremos los otros datos
-        new_client = ClientModel.find_by_email(new_email)
-
-        assert new_client.name == new_name
-        assert new_client.iban == new_iban
-        assert new_client.dni_nie == new_dni_nie
-        assert new_client.email == new_email
-
-
-def test_profile_modify_error1(): # Cuando no se encuentra el cliente de la url
-    with app.test_client() as c:
-        # Hacemos el put con datos inventados
-        path = '/profile/inventado@gmail.com'
-        params = {
-            'name': "new_name",
-            'iban': 1111,
-            'dni_nie': 111,
-            'email': "gmail"
-        }
-        r = c.put(path, json=params)
-
-        assert r.status_code == 404
+    assert new_client.name == new_name
+    assert new_client.iban == new_iban
+    assert new_client.dni_nie == new_dni_nie
+    assert new_client.email == new_email
 
 
-def test_profile_modify_error2(): # El cliente introduce un nuevo email ya en uso
-    with app.test_client() as c:
-        # Cogemos un cliente random de la base de datos.
-        client_old = ClientModel.query.filter(ClientModel.client_id == 1).first()
+def test_profile_modify_error1(client): # Cuando no se encuentra el cliente de la url
+    c = client
 
-        # Hacemos el put con datos inventados menos el email
-        path = '/profile/inventado@gmail.com'
-        params = {
-            'name': "new_name",
-            'iban': 1111,
-            'dni_nie': 111,
-            'email': client_old.email
-        }
-        r = c.put(path, json=params)
+    # Hacemos el put con datos inventados
+    path = '/profile/inventado@gmail.com'
+    params = {
+        'name': "new_name",
+        'iban': 1111,
+        'dni_nie': 111,
+        'email': "gmail"
+    }
+    r = c.put(path, json=params)
 
-        assert r.status_code == 405
+    assert r.status_code == 404
 
 
-def test_profile_modify_error3():  # El cliente introduce un nuevo dni_nie ya en uso
-    with app.test_client() as c:
-        # Cogemos un cliente random de la base de datos.
-        client_old = ClientModel.query.filter(ClientModel.client_id == 1).first()
+def test_profile_modify_error2(client): # El cliente introduce un nuevo email ya en uso
+    c = client
 
-        # Hacemos el put con datos inventados menos el dni
-        path = '/profile/inventado@gmail.com'
-        params = {
-            'name': "new_name",
-            'iban': 1111,
-            'dni_nie': client_old.dni_nie,
-            'email': "gmail"
-        }
-        r = c.put(path, json=params)
+    # Cogemos un cliente random de la base de datos.
+    client_old = ClientModel.query.filter(ClientModel.client_id == 1).first()
 
-        assert r.status_code == 406
+    # Hacemos el put con datos inventados menos el email
+    path = '/profile/inventado@gmail.com'
+    params = {
+        'name': "new_name",
+        'iban': 1111,
+        'dni_nie': 111,
+        'email': client_old.email
+    }
+    r = c.put(path, json=params)
+
+    assert r.status_code == 405
+
+
+def test_profile_modify_error3(client):  # El cliente introduce un nuevo dni_nie ya en uso
+    c = client
+
+    # Cogemos un cliente random de la base de datos.
+    client_old = ClientModel.query.filter(ClientModel.client_id == 1).first()
+
+    # Hacemos el put con datos inventados menos el dni
+    path = '/profile/inventado@gmail.com'
+    params = {
+        'name': "new_name",
+        'iban': 1111,
+        'dni_nie': client_old.dni_nie,
+        'email': "gmail"
+    }
+    r = c.put(path, json=params)
+
+    assert r.status_code == 406
 
 
 
