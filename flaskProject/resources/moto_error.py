1515c1ffec279496819eb2393c2a959f9b61c67c
Jose Blanco Fernandez <31952475+SrLimoncio@users.noreply.github.com>
2020-12-14 20:31:08 +0100

    funcionamiento correcto

--- flaskProject/resources/moto_error.py
+++ flaskProject/resources/moto_error.py
@@ -8,11 +8,17 @@ class MotoError(Resource):
     def post(self, moto_id):
         try:
             moto = MotoModel.find_by_id(moto_id)
-            if(moto is not None):
-                ReservedRunningModel.find_by_moto(moto_id)
-                moto.set_state("ALERT")
+            if moto is not None:
+                if moto.state == "RESERVED" or moto.state == "ACTIVE":
+                    rr = ReservedRunningModel.find_by_moto(moto.id)
+                    rr.delete_from_db()
+                    moto.set_state("ALERT")
+                    return {"message": "Correctly reported error"}, 200
+                else:
+                    moto.set_state("ALERT")
+                    return {"message": "Correctly reported error"}, 200
             else:
                 return {"message": "Moto not found"}, 404
-
         except:
             return {"message": "Internal error when reporting the error"}, 500
+
